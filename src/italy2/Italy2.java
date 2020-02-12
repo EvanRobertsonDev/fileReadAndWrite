1f873f3aeb17a7f75600225b1a6f59557accfcf3
evrob0095 <evrob0095@7L9N6Y1.Student.UGDSB.ED>
2020-02-12 12:43:14 -0500

    big brain commit

---
+++ src/italy2/Italy2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package italy2;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Properties;
+/**
+ *
+ * @author evrob0095
+ */
+public class Italy2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        readFile.getData("config.properties", "haha");
+        
+        
+        
+    }
+    
+}
