c74ae30e5b4a71905060ec3e6fc4bb3eec59fdff
evrob0095 <evrob0095@7L9N6Y1.Student.UGDSB.ED>
2020-02-12 12:47:10 -0500

    big brain commit 2

---
+++ src/italy2/readFile.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package italy2;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Properties;
+
+/**
+ *
+ * @author evrob0095
+ */
+public class readFile {
+    
+    public boolean getData(String fileName, String property) {
+        
+        String[] arr = new String[100];
+        boolean config = false;
+        FileReader prop = null;
+        try {
+            prop = new FileReader("config.properties");
+        } catch(FileNotFoundException e){
+            System.err.println(e);
+        }
+        BufferedReader buffer = new BufferedReader(prop);
+        for (int i = 0; i < 100; i++) {
+            try {
+                arr[i] = buffer.readLine();
+                if (arr[i].contains(property)) {
+                    if (arr[i].contains("true")) {
+                        config = true;
+                        break;
+                    }
+                    else if (arr[i].contains("false")) {
+                        config = false;
+                        break;
+                    }
+                    else {
+                        System.err.println("There was an issue readng the file.\n Terminating instance");
+                        i = 100;
+                    }
+                }
+                else {
+                    
+                }
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+        if(config == true) {
+             return(true);   
+        }
+        else {
+            return(false);
+        }
+        
+       
+    }   
+    
+}
